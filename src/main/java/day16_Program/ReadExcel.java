3a84e1fe9e98426a72b4b376cc0da5eff53431d9
nandhini <nandhinibalak92@gmail.com>
2025-05-12 17:55:03 +0530

    First commit

---
+++ src/main/java/day16_Program/ReadExcel.java
@@ -0,0 +1,56 @@
+package day16_Program;
+
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadExcel {
+
+	public static void main(String[] args) throws IOException
+	
+	{
+		XSSFWorkbook book = new XSSFWorkbook("C:\\\\Users\\\\Admin\\\\eclipse-workspace\\\\Maven_Files\\\\src\\\\main\\\\java\\\\WriteExcel.xlsx");
+		
+		XSSFSheet sheet = book.getSheet("LoginData");
+		
+		int rowCount = sheet.getLastRowNum();
+		
+		int columnCount = sheet.getRow(0).getLastCellNum();
+		
+		String[][] data = new String[rowCount][columnCount];
+		
+        for(int i=1; i<=rowCount; i++) {
+			
+			XSSFRow row = sheet.getRow(i);
+			
+         for(int j=0; j<columnCount ; j++) {
+				
+				XSSFCell cell = row.getCell(j);
+				
+				System.out.print(cell.getStringCellValue()+"|");
+				
+				data[i-1][j]=cell.getStringCellValue();
+				
+             }
+
+              System.out.println();
+                }
+               System.out.println();
+                System.out.println("Printing from array");
+
+                for(String[] row : data) {
+	
+	            for(String x : row) {
+		
+		           System.out.print(x +"|");
+	              }
+	               System.out.println();
+                  }
+
+                    book.close();
+	      }
+
+      }
---
+++ src/main/java/day16_Program/WriteExcel.java
@@ -0,0 +1,56 @@
+package day16_Program;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import java.io.FileOutputStream;
+
+public class WriteExcel {
+
+	public static void main(String[] args) throws Exception
+	{
+		XSSFWorkbook book = new XSSFWorkbook();
+		XSSFSheet sheet = book.createSheet("LoginData");
+		Object [][] data = {
+				{"Name", "Age", "Email"},
+				{"John Doe", "30", "john@test.com"},
+				{"Jane Doe", "28", "john@test.com"},
+				{"Bob Smith", "35", "jacky@example.com"},
+				{"Swapnil", "37", "swapnil@example.com"},
+		};
+		int rowCount =0;
+		
+		for(Object[] row1 : data) {
+			XSSFRow row = sheet.createRow(rowCount++);
+			
+			int columnCount=0;
+			
+			for (Object col: row1)
+			{
+				XSSFCell cell = row.createCell(columnCount++);
+				
+                   if(col instanceof String) {
+					
+					cell.setCellValue((String)col);
+             } else  if (col instanceof Integer) {
+					
+					cell.setCellValue((Integer)col);
+             
+		}
+
+
+	}
+
+}
+		
+		try {
+			FileOutputStream output = new FileOutputStream("C:\\Users\\Admin\\eclipse-workspace\\Maven_Files\\src\\main\\java\\WriteExcel.xlsx");
+			book.write(output);
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		book.close();
+}
+}
